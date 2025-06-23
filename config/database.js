850b5d37bb832ef3d678d925e3de584802364603
i-niranjan <niranjanchaudhari2004@gmail.com>
2025-06-23 21:28:17 +0530

    Updated database.js

--- config/database.js
+++ config/database.js
@@ -1,26 +1,15 @@
-module.exports = ({ env }) => {
-  const client = env("DATABASE_CLIENT", "postgres");
-
-  const connections = {
-    postgres: {
-      connection: {
-        connectionString: env("DATABASE_URL"), // Use the DATABASE_URL from Railway directly
-        ssl: {
-          rejectUnauthorized: false, // Important for Railway's PostgreSQL SSL connection
-        },
-      },
-      pool: {
-        min: env.int("DATABASE_POOL_MIN", 2),
-        max: env.int("DATABASE_POOL_MAX", 10),
-      },
-    },
-  };
-
-  return {
+module.exports = ({ env }) => ({
+  connection: {
+    client: "postgres",
     connection: {
-      client,
-      ...connections[client],
-      acquireConnectionTimeout: env.int("DATABASE_CONNECTION_TIMEOUT", 60000),
+      host: env("DATABASE_HOST"),
+      port: env.int("DATABASE_PORT"),
+      database: env("DATABASE_NAME"),
+      user: env("DATABASE_USERNAME"),
+      password: env("DATABASE_PASSWORD"),
+      ssl: {
+        rejectUnauthorized: false,
+      },
     },
-  };
-};
+  },
+});
